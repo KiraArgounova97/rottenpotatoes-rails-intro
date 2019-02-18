05599b8c10f6ac62e2c6d7f0adab797e059e1832
EC2 Default User <ec2-user@ip-172-31-16-194.us-east-2.compute.internal>
2019-02-18 22:40:58 +0000

    check

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,20 +24,43 @@ class MoviesController < ApplicationController
    elsif params[:sort_by] == 'release_date'
     @release_date_header = 'hilite'
     @movies = Movie.order(release_date: :asc)
-   else
-      # Part2: Filter the list of the movies ===========================
-      # 'ratings': Aggregate the values into a single hash
-      if params[:ratings]
-        @movies = Movie.where(:rating => params[:ratings].keys)
-        @checked_ratings = params[:ratings].keys
-      else
-        @movies = Movie.all
-        @checked_ratings = []
-      end
    end
+
+    # Part2: Filter the list of the movies ===========================
+    # 'ratings': Aggregate the values into a single hash
+    if params[:ratings]
+      @movies = Movie.where(:rating => params[:ratings].keys)
+      @checked_ratings = params[:ratings].keys
+      
+      if params[:sort_by] == 'title'
+        @title_header = 'hilite'
+        @movies = Movie.order(title: :asc)
+      elsif params[:sort_by] == 'release_date'
+        @release_date_header = 'hilite'
+        @movies = Movie.order(release_date: :asc)
+      end
+
+    else
+      @movies = Movie.all
+      @checked_ratings = []
+
+      if params[:sort_by] == 'title'
+        @title_header = 'hilite'
+        @movies = Movie.order(title: :asc)
+      elsif params[:sort_by] == 'release_date'
+        @release_date_header = 'hilite'
+        @movies = Movie.order(release_date: :asc)
+      end
+
+    end
+      
+      
+      
+      
+      
+      
+      
   end
-
-
 # ====================================================================
 
   def new
