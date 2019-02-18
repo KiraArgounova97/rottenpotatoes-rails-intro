3ab941a8aeb033f3fa01210bb32e3ef419d41ebf
EC2 Default User <ec2-user@ip-172-31-16-194.us-east-2.compute.internal>
2019-02-18 20:01:55 +0000

    part1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,9 +34,13 @@ class MoviesController < ApplicationController
     else
       @ratings = nil
     end
+    
+    @ratings.keys
+    
+    
 
-  
   end
+
 
 # ====================================================================
 
